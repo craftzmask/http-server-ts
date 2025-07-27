72b506dca2fc4079f5a38a995838f16a63377628
Khanh Chung <khanh.d.chung@gmail.com>
2025-07-26 21:48:55 -0700

    Extract URL path

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,17 @@
 import * as net from "net";
 
-const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+const server = net.createServer((socket: net.Socket) => {
+  socket.setEncoding('utf-8');
+  
+  socket.on("data", (data: Buffer<ArrayBufferLike>) => {
+    const requestLine: string = data.toString().split("\r\n")[0];
+    const [method, target, version] = requestLine.split(" ");
+    
+    if (target == "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
   });
 
   socket.on("close", () => {
