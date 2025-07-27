9e48b4c02a55e3bccf2be5bb406c56a88323b444
Khanh Chung <khanh.d.chung@gmail.com>
2025-07-26 23:03:00 -0700

    Respond with body

--- app/main.ts
+++ app/main.ts
@@ -7,11 +7,15 @@ const server = net.createServer((socket: net.Socket) => {
     const requestLine: string = data.toString().split("\r\n")[0];
     const [method, target, version] = requestLine.split(" ");
     
+    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
     if (target == "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      response = "HTTP/1.1 200 OK\r\n\r\n";
+    } else if (target.startsWith("/echo/")) {
+      const param: string = target.substring("/echo/".length);
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${param.length}\r\n\r\n${param}`
     }
+    
+    socket.write(response);
   });
 
   socket.on("close", () => {
